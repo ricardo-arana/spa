21d7a00599f722934e6077f695a84914572ec76e
Ricardo Arana Reyes Guerrero <ricardo.aranareyes@gmail.com>
2019-07-12 18:35:05 -0500

    55. rutas con parametros

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { NgModule } from '@angular/core';
 //rutas
 import { APP_ROUTING } from './app.routes';
 
-
-//componentes
+//servicios
+import { HeroesService } from './servicios/heroes.service';
+ //componentes
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
+import { HeroeComponent } from './components/heroe/heroe.component';
 
 @NgModule({
   declarations: [
@@ -18,13 +20,14 @@ import { HeroesComponent } from './components/heroes/heroes.component';
     NavbarComponent,
     HomeComponent,
     AboutComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroeComponent
   ],
   imports: [
     BrowserModule,
     APP_ROUTING
   ],
-  providers: [],
+  providers: [HeroesService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HeroesService, Heores } from '../../servicios/heroes.service';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-heroes',
@@ -6,10 +9,16 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class HeroesComponent implements OnInit {
-
-  constructor() { }
+   heroes:Heores[] = [];
+  constructor(private _heroesService: HeroesService,
+              private _router:Router) { }
 
   ngOnInit() {
+    this.heroes = this._heroesService.getHeroes();
+  }
+
+  verHeore(index:number){
+      this._router.navigate( ['/heroe',index] );
   }
 
 }
