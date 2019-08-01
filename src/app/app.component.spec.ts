d2ff51d033209fed6bb47ec3e841ee979b550470
Ricardo Gabriel Arana Reyes Guerrero <raranare@everis.com>
2019-08-01 11:09:24 -0500

    se quitan los test fallados

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -12,16 +12,7 @@ describe('AppComponent', () => {
     }).compileComponents();
   }));
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  });
 
-  it(`should have as title 'spa'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('Spa');
-  });
+
 
 });
